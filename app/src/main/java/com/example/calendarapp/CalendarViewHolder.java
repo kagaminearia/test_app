a1c59865ce189c9a3b01e193e701ae99d26c8ad3
kagaminearia <rinkitang@outlook.com>
2023-02-21 09:16:28 -0500

    first commit

---
+++ app/src/main/java/com/example/calendarapp/CalendarViewHolder.java
@@ -0,0 +1,28 @@
+package com.example.calendarapp;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public final View parentView;
+    public final TextView dayOfMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
+    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
+    {
+        super(itemView);
+        parentView = itemView.findViewById(R.id.parentView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
+    }
+}
