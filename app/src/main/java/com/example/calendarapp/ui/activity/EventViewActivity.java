147e40ea97280cfdd0f658a7a0075f11102747c0
preservedfish <71352883+preservedfish@users.noreply.github.com>
2023-03-09 01:07:15 -0500

    Add basic Firebase to project

--- app/src/main/java/com/example/calendarapp/ui/activity/EventViewActivity.java
+++ app/src/main/java/com/example/calendarapp/ui/activity/EventViewActivity.java
@@ -3,14 +3,22 @@ package com.example.calendarapp.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.calendarapp.CalendarAdapter;
 import com.example.calendarapp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class EventViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener{
+    private static final String TAG = "EventViewActivity";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +29,40 @@ public class EventViewActivity extends AppCompatActivity implements CalendarAdap
     }
 
     private void initWidgets() {
-        setContentView(R.layout.activity_event_view);
+        // Open Firebase connection and add test data
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("events");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.d(TAG, "New event successfully added");
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
 
+        Event testEvent = new Event("Checkpoint 4");
+        String testDate = "3-9-2023";
+        ref.child(testDate).setValue(testEvent);
     }
 
     @Override
     public void onItemClick(int position, LocalDate date) {
 
+    }
+
+    public static class Event {
+        public Date date;
+        public String name;
+        public Date startTime;
+        public Integer type;
+        public String info;
+
+        public Event() {
+        }
+
+        public Event(String name) {
+            this.name = name;
+        }
     }
 }
